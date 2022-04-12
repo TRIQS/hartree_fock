4f9704a9956073e681be826b53fa00a849cf4489
Jonathan Karp <jkarp314@gmail.com>
2022-04-12 16:15:20 -0400

    tests for single and multiband cases

--- test/python/LatticeSolver.py
+++ test/python/LatticeSolver.py
@@ -7,37 +7,11 @@ from triqs.gf import *
 from triqs.lattice import *
 from triqs.operators import *
 from hartree_fock.solver import flatten, unflatten
+from h5 import HDFArchive
+
 class test_lattice_solver(unittest.TestCase):
 
-    def test_run(self):
-
-        # BL = BravaisLattice(units = [(1,0,0) , (0,1,0) , (0,0,1)])
-        # BZ = BrillouinZone(BL)
-        # nk=10
-        # mk = MeshBrZone(BZ, nk)
-        # ekup = Gf(mesh = mk, target_shape=[2,2])
-        # ekdn = Gf(mesh = mk, target_shape=[2,2])
-
-        # for k in mk:
-        #     ekup[k][0,0] = 2*np.cos(k[0]) + np.cos(k[1]) + np.cos(k[2])
-        #     ekup[k][1,1] = np.cos(k[0]) + np.cos(k[1]) + np.cos(k[2])
-        #     ekup[k][0,1] = 0.1*np.cos(k[0])
-        #     ekup[k][1,0] = 0.1*np.cos(k[0])
-        #     ekdn[k] = (np.cos(k[0]) + np.cos(k[1]) + np.cos(k[2]) - 10) * np.identity(2)
-        #     ekdn[k][0,1] = 0.1*np.cos(k[1])
-        #     ekdn[k][1,0] = 0.1*np.cos(k[1])
-
-        # e_k = BlockGf(name_list = ['up', 'down'], block_list = [ekup, ekdn])
-        # gf_struct = [('up', 2), ('down', 2)]
-
-        # h_int = 3*n('up', 0)*n('down', 0) + 3*n('up', 1)*n('down', 1) + 2.5*n('up', 0)*n('down', 1)
-
-        # S = LatticeSolver(e_k=e_k, h_int=h_int, gf_struct=gf_struct, beta=40)
-        # S.solve(N_target=2, with_fock=True)
-        # print('Sigma_up = ', S.Sigma_HF['up'])
-        # print('Sigma_down = ', S.Sigma_HF['down'])
-        # print('mu = ', S.mu)
-        # print('rho = ', S.rho)
+    def test_one_band(self):
 
 
         BL = BravaisLattice(units = [(1,0,0) , (0,1,0) , (0,0,1)])
@@ -57,10 +31,48 @@ class test_lattice_solver(unittest.TestCase):
 
         S = LatticeSolver(e_k=e_k, h_int=h_int, gf_struct=gf_struct, beta=40)
         S.solve(N_target=1, with_fock=True)
-        print('Sigma_up = ', S.Sigma_HF['up'])
-        print('Sigma_down = ', S.Sigma_HF['down'])
-        print('mu = ', S.mu)
-        print('rho = ', S.rho)
+
+        with HDFArchive('one_band.ref.h5', 'r') as ar:
+            Sigma_ref = ar['Sigma']
+            mu_ref = ar['mu']
+        np.testing.assert_allclose(S.Sigma_HF['up'], Sigma_ref[0,0], rtol=0, atol=1e-10)
+        np.testing.assert_allclose(S.Sigma_HF['down'], Sigma_ref[1,1], rtol=0, atol=1e-10)
+        np.testing.assert_allclose(S.mu, mu_ref)
+
+    def test_multi_band(self):
+
+        BL = BravaisLattice(units = [(1,0,0) , (0,1,0) , (0,0,1)])
+        BZ = BrillouinZone(BL)
+        nk=10
+        mk = MeshBrZone(BZ, nk)
+        ekup = Gf(mesh = mk, target_shape=[2,2])
+        ekdn = Gf(mesh = mk, target_shape=[2,2])
+
+        for k in mk:
+            ekup[k][0,0] = 2*np.cos(k[0]) + np.cos(k[1]) + np.cos(k[2])
+            ekup[k][1,1] = np.cos(k[0]) + np.cos(k[1]) + np.cos(k[2])
+            ekup[k][0,1] = 0.1*np.cos(k[0])
+            ekup[k][1,0] = 0.1*np.cos(k[0])
+            ekdn[k] = (np.cos(k[0]) + np.cos(k[1]) + np.cos(k[2]) - 0.1) * np.identity(2)
+            ekdn[k][0,1] = 0.1*np.cos(k[1])
+            ekdn[k][1,0] = 0.1*np.cos(k[1])
+
+        e_k = BlockGf(name_list = ['up', 'down'], block_list = [ekup, ekdn])
+        gf_struct = [('up', 2), ('down', 2)]
+
+        h_int = 3*n('up', 0)*n('down', 0) + 3*n('up', 1)*n('down', 1) + 2.5*n('up', 0)*n('down', 1)
+
+        S = LatticeSolver(e_k=e_k, h_int=h_int, gf_struct=gf_struct, beta=40)
+        S.solve(N_target=2, with_fock=True)
+        with HDFArchive('multi_band.ref.h5', 'r') as ar:
+            Sigma_ref = ar['Sigma']
+            mu_ref = ar['mu']
+            rho_ref = ar['rho']
+        np.testing.assert_allclose(S.Sigma_HF['up'], Sigma_ref[:2,:2], rtol=0, atol=1e-5)
+        np.testing.assert_allclose(S.Sigma_HF['down'], Sigma_ref[2:,2:], rtol=0, atol=1e-5)
+        np.testing.assert_allclose(S.rho['up'], rho_ref[:2,:2], rtol=0, atol=1e-5)
+        np.testing.assert_allclose(S.rho['down'], rho_ref[2:,2:], rtol=0, atol=1e-5)
+        np.testing.assert_allclose(S.mu, mu_ref)
 
 if __name__ == '__main__':
     unittest.main()
